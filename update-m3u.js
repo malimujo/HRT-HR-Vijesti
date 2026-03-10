c2e5a59b7101ee44a070185f72ff5d5aba9bcfc1
malimujo <42038038+malimujo@users.noreply.github.com>
2026-03-10 08:12:38 +0100

    Fix date format in emisijaInfo string

--- update-m3u.js
+++ update-m3u.js
@@ -70,7 +70,7 @@ async function updateM3U() {
         const vrijeme = timeMatch[4];
         const sat = vrijeme.slice(0,2);
         const minute = vrijeme.slice(2,4);
-        emisijaInfo = `${dan}.${mjesec}-${sat}:${minute}`;
+        emisijaInfo = `${dan}.${mjesec}.${sat}:${minute}.`;
       }
       
       console.log('📅 Datum/vrijeme:', emisijaInfo);
