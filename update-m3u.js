609dff8ef4b59e33d7d792da2a91a55cd7b934d1
malimujo <42038038+malimujo@users.noreply.github.com>
2026-03-31 10:19:28 +0200

    Update update-m3u.js

--- update-m3u.js
+++ update-m3u.js
@@ -76,7 +76,7 @@ console.log('📅 Datum/vrijeme:', emisijaInfo);
 
 const imageUrl = result.image || 'https://radio.hrt.hr/favicon.ico';
 const m3uContent = `#EXTM3U
-#EXTINF:-1 tvg-logo="${imageUrl}" group-title="Vijesti",Vijesti ${emisijaInfo}
+#EXTINF:-1 tvg-logo="${imageUrl}" group-title="Slušaonica",Vijesti ${emisijaInfo}
 ${result.mp3}`;
 
 fs.writeFileSync('vijesti.m3u', m3uContent);
